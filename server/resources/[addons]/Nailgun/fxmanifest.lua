f5fa298b2f0cd8744d9f37a3ca490fd2e06e89ca
Lucas Hen <oisouhen@gmail.com>
2022-05-26 15:58:46 -0300

    Update [addons]

---
+++ server/resources/[addons]/Nailgun/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "bodacious"
+game "gta5"
+lua54 "yes"
+
+files {
+	"pedpersonality.meta",
+	"weaponanimations.meta",
+	"weaponarchetypes.meta",
+	"weapons_nailgun.meta"
+}
+
+data_file "PED_PERSONALITY_FILE" "pedpersonality.meta"
+data_file "WEAPON_ANIMATIONS_FILE" "weaponanimations.meta"
+data_file "WEAPON_METADATA_FILE" "weaponarchetypes.meta"
+data_file "WEAPONINFO_FILE" "weapons_nailgun.meta"
+data_file "WEAPONINFO_FILE_PATCH" "weapons_nailgun.meta"
