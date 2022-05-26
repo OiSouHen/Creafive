7de81c753a6f7b602fc6f29276306f019466c33b
Lucas Hen <oisouhen@gmail.com>
2022-05-26 15:51:38 -0300

    Create [addons]

---
+++ server/resources/[addons]/creativeSpeeds/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "bodacious"
+game "gta5"
+
+client_script "names.lua"
+
+files {
+	"data/*"
+}
+
+data_file "CARCOLS_FILE" "data/carcols.meta"
+data_file "HANDLING_FILE" "data/handling.meta"
+data_file "VEHICLE_METADATA_FILE" "data/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/carvariations.meta"
---
+++ server/resources/[addons]/creativeVehs/fxmanifest.lua
@@ -0,0 +1,49 @@
+fx_version "bodacious"
+game "gta5"
+
+client_script "names.lua"
+
+files {
+	"data/*",
+	"audio/*",
+	"audio/**/*"
+}
+
+data_file "CARCOLS_FILE" "data/carcols.meta"
+data_file "POPSCHED_FILE" "data/popcycle.dat"
+data_file "HANDLING_FILE" "data/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/carvariations.meta"
+
+data_file "AUDIO_WAVEPACK" "audio/lfasound/dlc_lfasound"
+data_file "AUDIO_GAMEDATA" "audio/lfasound/lfasound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/lfasound/lfasound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/r35sound/dlc_r35sound"
+data_file "AUDIO_GAMEDATA" "audio/r35sound/r35sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/r35sound/r35sound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/r34sound/dlc_r34sound"
+data_file "AUDIO_GAMEDATA" "audio/r34sound/r34sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/r34sound/r34sound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/carrerav10/dlc_carrerav10"
+data_file "AUDIO_GAMEDATA" "audio/carrerav10/carrerav10_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/carrerav10/carrerav10_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/huracanv10/dlc_huracanv10"
+data_file "AUDIO_GAMEDATA" "audio/huracanv10/huracanv10_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/huracanv10/huracanv10_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/hemisound/dlc_hemisound"
+data_file "AUDIO_GAMEDATA" "audio/hemisound/hemisound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/hemisound/hemisound_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/argento/dlc_argento"
+data_file "AUDIO_GAMEDATA" "audio/argento/argento_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/argento/argento_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/chevroletlt4/dlc_chevroletlt4"
+data_file "AUDIO_GAMEDATA" "audio/chevroletlt4/chevroletlt4_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/chevroletlt4/chevroletlt4_sounds.dat"
