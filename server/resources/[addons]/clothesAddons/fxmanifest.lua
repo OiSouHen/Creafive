d8384f283c9b1918f052efad1e89ab7a7b4aa8d6
Lucas Hen <oisouhen@gmail.com>
2022-05-26 15:56:15 -0300

    Update [addons]

---
+++ server/resources/[addons]/clothesAddons/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "bodacious"
+game "gta5"
+
+files {
+	"data/*",
+	"data/**/*"
+}
+
+data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_f_freemode_01_mp_f_police.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_m_freemode_01_mp_m_police.meta"
+
+data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_f_freemode_01_mp_f_standard.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_m_freemode_01_mp_m_standard.meta"
