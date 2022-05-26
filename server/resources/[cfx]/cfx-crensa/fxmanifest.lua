ff74b5fd11f1cf864597828c6a08e191492832fe
Lucas Hen <oisouhen@gmail.com>
2022-05-26 15:44:44 -0300

    Create [cfx]

---
+++ server/resources/[cfx]/cfx-crensa/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "cerulean"
+game "gta5"
+
+this_is_a_map "yes"
+
+files {
+	"stream/*",
+	"stream/*/**",
+	"stream/*/*/**",
+	"stream/*/*/*/**",
+	"interiorproxies.meta"
+}
+
+data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
+data_file "DLC_ITYP_REQUEST" "stream/creativeBadges/v_badges.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/creativeInteriores/creative_props.ytyp"
